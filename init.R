2e1979b1ccd2d40ec1b542de7f664a8e8e221df3
luke <luke@amitree.com>
2018-04-06 21:57:58 -0700

    incremental test of additional package install on  init file for pa-r-abox

--- init.R
+++ init.R
@@ -18,29 +18,32 @@ my_packages <- c("plyr"
             ,"glmnet"
             ,"mfp"
             ,"randomForest"
+            ,"tm"            
             )
 
             # , "caret"
             # , "FactoMineR"
             # ,"text2vec"
-            # ,"tm"
+
             # ,"RPostgreSQL"
 
 #my_github_packages <- c('bmschmidt/wordVectors', 'rstudio/tensorflow')
 
 install_if_missing = function(p) {
-  print(paste("installing", p))
-
   if (p %in% rownames(installed.packages()) == FALSE) {
+    cat(paste("Installing package:", p, "\n"))
     install.packages(p)
+  }
+  else {
+    cat(paste("Skipping already installed package:", p, "\n"))
   }
 }
 
 # TODO add invisible to both vvvvvv
 sapply(my_packages, install_if_missing)
-sapply(my_packages, function(x) {
-    library(x, character.only = TRUE)
-})
+#sapply(my_packages, function(x) {
+#    library(x, character.only = TRUE)
+#})
 #sapply(my_github_packages, function(x){devtools::install_github(x)})
 #sapply(my_github_packages, function(x) {
 #    library(x, character.only = TRUE)
